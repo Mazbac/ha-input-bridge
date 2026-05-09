28673f929e47e0b5903dfd909ed22c85db271954
Mertcan Ozbek <94199079+Mazbac@users.noreply.github.com>
2026-05-09 22:42:28 +0200

    Add constants

---
+++ custom_components/ha_input_bridge/const.py
@@ -0,0 +1,14 @@
+"""Constants for the HA Input Bridge integration."""
+
+from __future__ import annotations
+
+DOMAIN = "ha_input_bridge"
+
+CONF_HOST = "host"
+CONF_PORT = "port"
+CONF_TOKEN = "token"
+CONF_NAME = "name"
+
+DEFAULT_NAME = "HA Input Bridge"
+DEFAULT_PORT = 8765
+DEFAULT_TIMEOUT = 5
